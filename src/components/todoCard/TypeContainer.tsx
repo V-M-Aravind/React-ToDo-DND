4e9f8781f426c68be3e98dd8bc1524f58734a70a
V M Aravind <vmaravind07@gmail.com>
2024-01-26 00:37:33 +0530

    Removed unused variables

--- src/components/todoCard/TypeContainer.tsx
+++ src/components/todoCard/TypeContainer.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { DropTargetMonitor, useDrop } from 'react-dnd';
-import { ItemProps, MOVABLE_TYPE, ToDo } from '../model/model';
-import './TypeContainer.css';
+import React from "react";
+import { DropTargetMonitor, useDrop } from "react-dnd";
+import { ItemProps, MOVABLE_TYPE } from "../model/model";
+import "./TypeContainer.css";
 type Props = {
   children: React.ReactNode;
   title: string;
   onDrop: (tile: string, item: ItemProps, monitor: DropTargetMonitor) => void;
 };
 function TypeContainer({ children, title, onDrop }: Props) {
-  const [{ canDrop, isOver }, drop] = useDrop(() => ({
+  const [{ isOver }, drop] = useDrop(() => ({
     accept: MOVABLE_TYPE,
     drop: (item: ItemProps, monitor) => {
       onDrop(title, item, monitor);
@@ -18,11 +18,11 @@ function TypeContainer({ children, title, onDrop }: Props) {
       canDrop: monitor.canDrop(),
     }),
   }));
-  const highlight = isOver ? 'highlight' : 'normal-bg';
+  const highlight = isOver ? "highlight" : "normal-bg";
   return (
     <div className={`type-container ${highlight}`} ref={drop}>
       <h2>{title}</h2>
-      <div className='children'>{children}</div>
+      <div className="children">{children}</div>
     </div>
   );
 }
